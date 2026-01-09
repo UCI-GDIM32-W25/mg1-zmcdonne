e6b309e38cb61c9bec4194e88591af1d02b76565
Zoya <zmcdonne@uci.edu>
2026-01-08 22:14:44 -0800

    rah

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,22 +7,23 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
-
+     public GameObject spritePrefab;
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
     private void Start ()
     {
-        
+    //player movement
     }
 
     private void Update()
     {
-        
+      //check updatate frame every scene count seeds in   
     }
 
     public void PlantSeed ()
     {
-        
+        if(Input.GetKeyDown);
+//instatiate the seed when playing space        
     }
 }
